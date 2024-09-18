7c4ee704466862ae782c0f19d4fa09b9f3823394
dong.nguyen <dong.nguyen@smartinnotech.io>
2024-09-18 11:43:05 +0700

    Initial Commit

---
+++ ToDoList_iOS/ToDoList_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_iOSApp.swift
+//  ToDoList_iOS
+//
+//  Created by macOS on 18/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
